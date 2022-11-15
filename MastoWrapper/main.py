b2e272b4103524ccf20ad85fcd2917d2c01ce23d
romulushill <romulushill@robottik.co.uk>
2022-11-15 21:06:03 +0000

    Fixed authentication and added toot() function

--- MastoWrapper/main.py
+++ MastoWrapper/main.py
@@ -16,57 +16,28 @@ import socket
 
 class Client:
     def __init__(self,name=None,token=None,address=None,scopes=None):
-        self.active = False
-        self.address = address
-        self.directions = {
-            "create_app":f"{address}/api/v1/apps",
-            "verify_credentials":f"{address}/api/v1/apps/verify_credentials",
-            "auth":f"{address}/oauth/authorize?",
-            "token":f"{address}/oauth/token",
-            "account":f"{address}/api/v1/accounts"
-        }
-        url1 = self.directions["create_app"]
-        params = {"client_name": f"{name}","redirect_uris":"urn:ietf:wg:oauth:2.0:oob","scopes":"write:accounts read"}
-        app_response = requests.post(url1, data=params)
-        if app_response.status_code == 200:
-            print("Returned 200")
-            print(app_response)
-            self.app_instance = app_response.json()
-            ## Created app instance
-            ## Preparing to generate oauth tokens from app instance
-            client_id = str(self.app_instance['client_id'])
-            client_secret = str(self.app_instance['client_secret'])
-            # Now we have the client id, the client secret, and finally the access token from the init all prepared.
-            url2 = self.directions["token"]
-            params = {"grant_type": "client_credentials","client_id": client_id,"client_secret":client_secret,"redirect_uri":"urn:ietf:wg:oauth:2.0:oob","scope":"write:accounts read"}
-            oauth_response = requests.post(url2, data=params)
-            print(oauth_response)
-            print("Here 1")
-            if oauth_response.status_code == 200:
-                print(oauth_response.json()["access_token"])
-                temp_access = str(oauth_response.json()["access_token"])
-                self.key = temp_access
-                #url3 = self.directions["account"]
-                #headings = {"Authorization":f"Bearer {temp_access}"}
-                #final_response = requests.post(url3, data=params, headers = headings)
-                #print(final_response.json())
-                #print("Here 2")
-                #print(oauth_response.history[0].url)
-
-                headings = {"Authorization":f"Bearer {token}"}
-                final_response = requests.get(f"{address}/api/v1/accounts/verify_credentials", headers = headings)
-                print(final_response.json())
-
-
-                print("COMPLETED AUTHENTICATION!")
-                self.active = True
-            else:
-                print("Failed to generate oauth token from app data")
-            #auth = {"Authorization": f"Bearer {token}"}
-            #response = requests.post(url, data=params, headers=auth)
-            self.active = True
-        else:
+        try:
             self.active = False
+            self.address = address
+            self.token = token
+            self.directions = {
+                "create_app":f"{address}/api/v1/apps",
+                "verify_credentials":f"{address}/api/v1/apps/verify_credentials",
+                "auth":f"{address}/oauth/authorize?",
+                "token":f"{address}/oauth/token",
+                "account":f"{address}/api/v1/accounts"
+            }
+            headings = {"Authorization":f"Bearer {token}"}
+            final_response = requests.get(f"{address}/api/v1/accounts/verify_credentials", headers = headings)
+            if final_response.status_code == 200:
+                self.active = True
+                return 200
+            else:
+                self.active = False
+        except Exception as e:
+            self.active = False
+            print(e)
+            return e
 
 
     def account_info(self):
@@ -77,7 +48,7 @@ class Client:
 
     def toot(self,word):
         if self.active == True:
-            headers = {"Authorization":f"Bearer {self.key}"}
+            headers = {"Authorization":f"Bearer {self.token}"}
             params = {"status": word}
             oauth_response = requests.post(f"{self.address}/api/v1/statuses", data=params,headers=headers)
             print(oauth_response)
