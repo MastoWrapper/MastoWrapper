b3aceca2010c3eaa83626d77a0098d1b89caffde
romulushill <romulushill@robottik.co.uk>
2022-11-15 17:28:28 +0000

    Appended web address present in __init__.py file

--- MastoWrapper/__init__.py
+++ MastoWrapper/__init__.py
@@ -1,6 +1,6 @@
 from verefa.main import machine
 
 __all__ = ['datetime','json','requests','time','sys','os','socket',"verefa"]
-__site__ = "https://verefa.com/"
+__site__ = "https://github.com/romulushill/MastoWrapper"
 __license__ = "MIT"
 __status__ = "Operational"
