4663f2d516e27182e81a0ed45094f6d9a7bda6d8
romulushill <47396684+romulushill@users.noreply.github.com>
2022-11-15 09:25:38 +0000

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,6 @@
+from verefa.main import machine
+
+__all__ = ['datetime','json','requests','time','sys','os','socket']
+__site__ = "https://verefa.com/"
+__license__ = "MIT"
+__status__ = "Operational"
